a502eeb60fcf2698351f8ac7b00758dd8050af0a
JKBoy2244 <jobayer2006@gmail.com>
2025-12-21 22:34:04 +0000

    Update Introduction.java

--- Classes/Introduction.java
+++ Classes/Introduction.java
@@ -1,3 +1,9 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.Random;
+
+
 class Introduction {
     public void messageDisplay() {
         System.out.println("This is the JK Bank App! Welcome and let's get you registered!");
