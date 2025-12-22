8b4c7583810bd940543605c84f8458b66c57f991
JKBoy2244 <jobayer2006@gmail.com>
2025-12-22 15:48:35 +0000

    Update PasswordTest.java

--- Tests/PasswordTest.java
+++ Tests/PasswordTest.java
@@ -8,5 +8,13 @@ public class PasswordTest {
   public static void main(String[] args) {
 
     Password register = new Password("XYZ1245SD", 26, "Female", "03456434454", "Xytre@hotmail.com","ABCDEFG1234", "ABCDEFG1234", 888888888);
+    System.out.println(register.getUsername());
+    System.out.println(register.getAge());
+    System.out.println(register.getGender());
+    System.out.println(register.getPhoneNumber());
+    System.out.println(register.getEmailAddress());
+    System.out.println(register.getPassword());
+    System.out.println(register.getConfirmPassword());
+    System.out.println(register.getPassCode());
   }
 }
