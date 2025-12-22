63622a7f9fade437385c1ff6531a82cf6f948100
JKBoy2244 <jobayer2006@gmail.com>
2025-12-22 15:29:26 +0000

    Update PasswordTest.java

--- Tests/PasswordTest.java
+++ Tests/PasswordTest.java
@@ -1,1 +1,12 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.Random;
 
+class PasswordTest {
+
+  public static void main(String[] args) {
+
+    Password register = new Password("XYZ1245SD", 26, "Female", "03456434454", "Xytre@hotmail.com","ABCDEFG1234", "ABCDEFG1234", 888888888);
+  }
+}
