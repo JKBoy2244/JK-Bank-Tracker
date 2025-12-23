6d29da84a4acfc29c52b64e402d51d6cdd4eae73
JKBoy2244 <jobayer2006@gmail.com>
2025-12-23 22:32:30 +0000

    Create InitialMoneyTransferTest2.java

---
+++ Tests/InitialMoneyTransferTest2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.Random;
+
+public class InitialMoneyTransferTest2 {
+
+  public static void main(String[] args) {
+
+    InitialMoneyTransfer money = new InitialMoneyTransfer(0.00);
+  }
+}
