5d3ae25799c9282e58d9806c2a79b8d96f005367
JKBoy2244 <jobayer2006@gmail.com>
2025-12-23 22:32:00 +0000

    Update and rename InitialMoneyTransferTest.java to InitialMoneyTransferTest1.java

--- Tests/InitialMoneyTransferTest1.java
+++ Tests/InitialMoneyTransferTest1.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 import java.util.LinkedHashSet;
 import java.util.Random;
 
-public class InitialMoneyTransferTest {
+public class InitialMoneyTransferTest1 {
 
   public static void main(String[] args) {
 
