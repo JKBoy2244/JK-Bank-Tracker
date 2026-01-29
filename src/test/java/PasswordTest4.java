7e1b62c10e4d1c8a51be1c51310ffbedfaed072e
JKBoy2244 <jobayer2006@gmail.com>
2026-01-29 23:01:21 +0000

    Update PasswordTest4.java

--- src/test/java/PasswordTest4.java
+++ src/test/java/PasswordTest4.java
@@ -1,3 +1,6 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.LinkedHashSet;
@@ -5,16 +8,17 @@ import java.util.Random;
 
 public class PasswordTest4 {
 
-  public static void main(String[] args) {
+  @Test
+  void constructor_sets_all_fields_correctly( {
 
     Password register = new Password("XYZ", 34444444, "boy", "0345643445455545435334345353353w", "Xytre@.com","ABCDE", "ABCDEFG1234666666", 888888888);
-    System.out.println(register.getUserName());
-    System.out.println(register.getAge());
-    System.out.println(register.getGender());
-    System.out.println(register.getPhoneNumber());
-    System.out.println(register.getEmailAddress());
-    System.out.println(register.getPassword());
-    System.out.println(register.getConfirmPassword());
-    System.out.println(register.getPassCode());
+    assertEquals("XYZ", register.getUserName());
+    assertEquals(34444444, register.getAge());
+    assertEquals("boy", register.getGender());
+    assertEquals("0345643445455545435334345353353w", register.getPhoneNumber());
+    assertEquals("Xytre@.com", register.getEmailAddress());
+    assertEquals("ABCDE", register.getPassword());
+    assertEquals("ABCDEFG1234666666", register.getConfirmPassword());
+    assertEquals(888888888, register.getPassCode());
   }
 }
