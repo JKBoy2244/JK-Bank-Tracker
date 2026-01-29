74a8558b9c91aadca91f8d8c6aab2709728f7458
JKBoy2244 <jobayer2006@gmail.com>
2026-01-29 22:57:29 +0000

    Update PasswordTest2.java

--- src/test/java/PasswordTest2.java
+++ src/test/java/PasswordTest2.java
@@ -1,3 +1,6 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.LinkedHashSet;
@@ -5,16 +8,17 @@ import java.util.Random;
 
 public class PasswordTest2 {
 
-  public static void main(String[] args) {
+  @Test
+  void constructor_sets_all_fields_correctly() {
 
     Password register = new Password("", 26, "", "", "","", "", 0);
-    System.out.println(register.getUserName());
-    System.out.println(register.getAge());
-    System.out.println(register.getGender());
-    System.out.println(register.getPhoneNumber());
-    System.out.println(register.getEmailAddress());
-    System.out.println(register.getPassword());
-    System.out.println(register.getConfirmPassword());
-    System.out.println(register.getPassCode());
+    assertEquals("", register.getUserName());
+    assertEquals(26, register.getAge());
+    assertEquals("", register.getGender());
+    assertEquals("", register.getPhoneNumber());
+    assertEquals("", register.getEmailAddress());
+    assertEquals("", register.getPassword());
+    assertEquals("", register.getConfirmPassword());
+    assertEquals(888888888, register.getPassCode());
   }
 }
