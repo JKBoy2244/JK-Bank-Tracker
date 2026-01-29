fc6b845bf2e10e8e9def058f805f85c6b3490546
JKBoy2244 <jobayer2006@gmail.com>
2026-01-29 22:49:57 +0000

    Update InitialMoneyTransferTest2.java

--- src/test/java/InitialMoneyTransferTest2.java
+++ src/test/java/InitialMoneyTransferTest2.java
@@ -1,3 +1,6 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.LinkedHashSet;
@@ -8,5 +11,6 @@ public class InitialMoneyTransferTest2 {
   public static void main(String[] args) {
 
     InitialMoneyTransfer money = new InitialMoneyTransfer(0.00);
+    assertEquals(0.00, getMoney());
   }
 }
